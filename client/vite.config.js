16c7e7cf94f15b4e2857c036d603c553ecaf30cb
Kevin Inzelrac <kevininzelrac@me.com>
2023-02-20 14:04:32 +0100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    proxy: {
+      "/api": "http://localhost:5001",
+    },
+  },
+  /* server: {
     host: true,
     open: true,
     port: 5000,
@@ -15,5 +20,5 @@ export default defineConfig({
         secure: false,
       },
     },
-  },
+  }, */
 });
