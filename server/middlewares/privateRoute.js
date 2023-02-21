69bbe8aba92ff65af05eb43f82d63154f7bb6926
Kevin Inzelrac <kevininzelrac@me.com>
2023-02-21 12:42:19 +0100

    Update privateRoute.js

--- server/middlewares/privateRoute.js
+++ server/middlewares/privateRoute.js
@@ -7,32 +7,44 @@ const { sign, verify } = require("jsonwebtoken");
 const privateRoute = async (req, res, next) => {
   try {
     const authHeader = await req.headers["authorization"];
+    console.log("privateRoute authHeader : " + authHeader);
     const accessToken = await authHeader?.split(" ")[1];
     const verifiedAccess = verify(accessToken, process.env.ACCESS_TOKEN);
-    //if (verifiedAccess) {
-    req.accessToken = accessToken;
-    req.user = verifiedAccess;
-    return next();
-    //}
+    if (verifiedAccess) {
+      req.accessToken = await accessToken;
+      req.user = verifiedAccess;
+
+      console.log("privateRoute verifiedAccessToken :" + verifiedAccess);
+
+      return next();
+    }
   } catch (err) {
     try {
       const refreshToken = await req.cookies["refreshToken"];
       const verifiedRefresh = verify(refreshToken, process.env.REFRESH_TOKEN);
-      //if (verifiedRefresh) {
-      const mongo = await UsersModel.findOne({
-        name: verifiedRefresh.name,
-        refreshToken: refreshToken,
-      });
-      const newAccessToken = sign(
-        { name: mongo.name },
-        process.env.ACCESS_TOKEN,
-        { expiresIn: "1m" }
-      );
-      req.accessToken = newAccessToken;
-      //req.user = verifiedRefresh;
-      req.user = mongo;
-      return next();
-      //}
+
+      console.log("privateRoute verifiedRefreshToken :");
+      console.log(verifiedRefresh);
+
+      if (verifiedRefresh) {
+        const mongo = await UsersModel.findOne(
+          {
+            name: await verifiedRefresh.name,
+            refreshToken: refreshToken,
+          },
+          { password: 0, __v: 0 }
+        );
+        const newAccessToken = sign(
+          { name: mongo.name },
+          process.env.ACCESS_TOKEN,
+          { expiresIn: "1m" }
+        );
+        req.accessToken = newAccessToken;
+        req.user = mongo;
+        return next();
+      } else {
+        throw new Error("RefreshToken unvalid");
+      }
     } catch (error) {
       res.json({
         error: "Not Allowed, please Log in or Register",
@@ -42,3 +54,21 @@ const privateRoute = async (req, res, next) => {
 };
 
 module.exports = { privateRoute };
+
+/*
+AccessToken = Token in Bearer 
+if( Verified AccessToken ) {
+  re.user = AccessToken
+  Next 
+}else if( !AccessToken) {
+  RefresToken = Token in Cookie
+  if( Verified RefreshToken ) {
+    if (verifiedToken = mongo.token) {
+    create NewAccessToken
+    Next
+    }else{
+    Not Allowed
+    }
+  }
+}
+*/
