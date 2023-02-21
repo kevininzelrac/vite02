7375a9fa4bf109355f6c9acdbcbc76f5c60c5e84
Kevin Inzelrac <kevininzelrac@me.com>
2023-02-21 14:53:09 +0100

    Update logout.js

--- server/routes/logout.js
+++ server/routes/logout.js
@@ -9,18 +9,22 @@ mongoose.set("strictQuery", false);
 const UsersModel = require("../models/users");
 
 router.get("/api/logout", async (req, res) => {
-  const refreshToken = await req.cookies["refreshToken"];
-  const user = verify(refreshToken, process.env.REFRESH_TOKEN);
-  await UsersModel.updateOne(
-    { name: user.name },
-    { $set: { refreshToken: "" } }
-  );
+  try {
+    const refreshToken = await req.cookies["refreshToken"];
+    const user = verify(refreshToken, process.env.REFRESH_TOKEN);
+    await UsersModel.updateOne(
+      { name: await user.name },
+      { $set: { refreshToken: "" } }
+    );
 
-  res.cookie("refreshToken", "none", {
-    maxAge: new Date(Date.now() + 5 * 1000),
-    httpOnly: true,
-  });
-  res.status(200).json("User logged out successfully");
+    res.cookie("refreshToken", "none", {
+      maxAge: new Date(Date.now() + 5 * 1000),
+      httpOnly: true,
+    });
+    res.send({ message: "User logged out successfully" });
+  } catch (error) {
+    res.send({ error: error });
+  }
 });
 
 module.exports = router;
