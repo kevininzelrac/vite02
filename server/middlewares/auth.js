2f526b3070f48586dab0acb2e72fe35f329a6003
Kevin Inzelrac <kevininzelrac@me.com>
2023-02-21 12:42:21 +0100

    Update auth.js

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -5,11 +5,11 @@ const UsersModel = require("../models/users");
 const bcrypt = require("bcrypt");
 
 const auth = async (req, res, next) => {
-  const user = await UsersModel.findOne({ name: req.body.name });
+  const user = await UsersModel.findOne({ name: await req.body.name });
 
   if (!user) return res.status(402).json({ error: "User Doesn't Exist" });
 
-  let match = await bcrypt.compare(req.body.password, user.password);
+  let match = await bcrypt.compare(await req.body.password, user.password);
 
   if (!match)
     return res
