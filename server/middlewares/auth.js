def14546d85a30b35f942894ca8fe9e0e644b5ba
Kevin Inzelrac <kevininzelrac@me.com>
2023-02-21 14:53:33 +0100

    Update auth.js

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -7,14 +7,14 @@ const bcrypt = require("bcrypt");
 const auth = async (req, res, next) => {
   const user = await UsersModel.findOne({ name: await req.body.name });
 
-  if (!user) return res.status(402).json({ error: "User Doesn't Exist" });
+  if (!user) return res.status(404).send({ error: "User Doesn't Exist" });
 
   let match = await bcrypt.compare(await req.body.password, user.password);
 
   if (!match)
     return res
-      .status(400)
-      .json({ error: "Wrong Username and Password Combination" });
+      .status(401)
+      .send({ error: "Wrong Username and Password Combination" });
 
   req.user = user;
   return next();
