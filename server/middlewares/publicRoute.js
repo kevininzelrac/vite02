b0c7a82105b5af2fe7007576b8683162951cfd41
Kevin Inzelrac <kevininzelrac@me.com>
2023-02-21 12:42:16 +0100

    Update publicRoute.js

--- server/middlewares/publicRoute.js
+++ server/middlewares/publicRoute.js
@@ -9,36 +9,43 @@ const publicRoute = async (req, res, next) => {
     const authHeader = await req.headers["authorization"];
     const accessToken = await authHeader?.split(" ")[1];
     const verifiedAccess = verify(accessToken, process.env.ACCESS_TOKEN);
-    //if (verifiedAccess) {
-    req.accessToken = accessToken;
-    req.user = verifiedAccess;
-    return next();
-    //}
+    if (verifiedAccess) {
+      req.accessToken = await accessToken;
+      req.user = verifiedAccess;
+
+      console.log("publicRoute verifiedAccessToken :" + verifiedAccess);
+
+      return next();
+    }
   } catch (err) {
     try {
       const refreshToken = await req.cookies["refreshToken"];
       const verifiedRefresh = verify(refreshToken, process.env.REFRESH_TOKEN);
-      //if (verifiedRefresh) {
-      const mongo = await UsersModel.findOne(
-        {
-          name: verifiedRefresh.name,
-          refreshToken: refreshToken,
-        },
-        { password: 0 }
-      );
-      const newAccessToken = sign(
-        { name: mongo.name },
-        process.env.ACCESS_TOKEN,
-        { expiresIn: "1m" }
-      );
-      req.accessToken = newAccessToken;
-      //req.user = verifiedRefresh;
-      req.user = mongo;
-      return next();
-      //}
+
+      console.log("publicRoute verifiedRefreshToken :");
+      console.log(verifiedRefresh);
+
+      if (verifiedRefresh) {
+        const mongo = await UsersModel.findOne(
+          {
+            name: await verifiedRefresh.name,
+            refreshToken: refreshToken,
+          },
+          { password: 0 }
+        );
+        const newAccessToken = sign(
+          { name: mongo.name },
+          process.env.ACCESS_TOKEN,
+          { expiresIn: "1m" }
+        );
+        req.accessToken = newAccessToken;
+        req.user = mongo;
+        return next();
+      }
     } catch (error) {
       req.accessToken = "";
       req.user = "";
+      console.log("poblicRoute : no user");
       return next();
       /* res.json({
         error: "Not Allowed, please Log in or Register",
