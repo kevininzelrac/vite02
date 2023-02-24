33446b06f7df38dc716a6eaf6f7d9c49bcca9259
Kevin Inzelrac <kevininzelrac@me.com>
2023-02-24 19:22:07 +0100

    Update unlikeComment.js

--- server/routes/unlikeComment.js
+++ server/routes/unlikeComment.js
@@ -1,15 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
-mongoose.connect(process.env.HOST);
-mongoose.set("strictQuery", false);
 const CommentsModel = require("../models/comments");
-//const { verifyToken } = require("../middlewares/verifyToken");
 const { privateRoute } = require("../middlewares/privateRoute");
-/* const cookieParser = require("cookie-parser");
-router.use(cookieParser()); */
 
-router.post("/api/unlikeComment", privateRoute, async (req, res) => {
+router.post("/", privateRoute, async (req, res) => {
   try {
     const comment = await CommentsModel.findOne({
       _id: await req.body._id,
