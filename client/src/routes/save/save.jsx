9bd5f01b856247ba29bec8bd0731e25f4740b531
Kevin Inzelrac <kevininzelrac@me.com>
2023-02-24 19:23:00 +0100

    Update save.jsx

--- client/src/routes/save/save.jsx
+++ client/src/routes/save/save.jsx
@@ -1,7 +1,7 @@
 export async function saveAction({ request }) {
   const formData = await request.formData();
   const update = Object.fromEntries(formData);
-  const response = await fetch("/api/updatepage", {
+  const response = await fetch("/api/updatePage", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     credentials: "include",
