e849412588cab7e7fd9137039113a1b0881e996b
Kevin Inzelrac <kevininzelrac@me.com>
2023-02-24 19:22:05 +0100

    Update updatepage.js

--- server/routes/updatepage.js
+++ server/routes/updatepage.js
@@ -1,15 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
-mongoose.connect(process.env.HOST);
-mongoose.set("strictQuery", false);
 const PostsModel = require("../models/posts");
-//const { verifyToken } = require("../middlewares/verifyToken");
 const { privateRoute } = require("../middlewares/privateRoute");
-/* const cookieParser = require("cookie-parser");
-router.use(cookieParser()); */
 
-router.post("/api/updatepage", privateRoute, async (req, res) => {
+router.post("/", privateRoute, async (req, res) => {
   try {
     const post = await PostsModel.findOne({
       author: await req.user.name,
