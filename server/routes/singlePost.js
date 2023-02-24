9c03ca3d5e7940581ec5d43bb42ff5e2ff2d9900
Kevin Inzelrac <kevininzelrac@me.com>
2023-02-24 19:22:09 +0100

    Update singlePost.js

--- server/routes/singlePost.js
+++ server/routes/singlePost.js
@@ -1,15 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
-mongoose.connect(process.env.HOST);
-mongoose.set("strictQuery", false);
-const PostsModel = require("./../models/posts");
-/* const { publicRoute } = require("../middlewares/publicRoute");
-const cookieParser = require("cookie-parser");
-router.use(cookieParser()); */
 
-/* router.get("/api/pages/:page", publicRoute, async (req, res) => { */
-router.get("/api/singlePost/:post", async (req, res) => {
+const PostsModel = require("./../models/posts");
+
+router.get("/:post", async (req, res) => {
   try {
     const post = await PostsModel.findOne({
       type: "post",
