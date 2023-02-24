25beba9e2dc4710733841394c2bba49329e502af
Kevin Inzelrac <kevininzelrac@me.com>
2023-02-24 19:22:00 +0100

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
-mongoose.connect(process.env.HOST);
-mongoose.set("strictQuery", false);
-const UsersModel = require("../models/users");
 const { privateRoute } = require("../middlewares/privateRoute");
-/* const cookieParser = require("cookie-parser");
-router.use(cookieParser()); */
 
-router.get("/api/user/", privateRoute, async (req, res) => {
+router.get("/", privateRoute, async (req, res) => {
   try {
     res.send({ user: await req.user });
   } catch (error) {
