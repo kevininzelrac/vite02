e6bab4fac51fd28d852bb5d6a5f9f260bc403db6
Kevin Inzelrac <kevininzelrac@me.com>
2023-02-24 19:22:30 +0100

    Update category.js

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,15 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
-mongoose.connect(process.env.HOST);
-mongoose.set("strictQuery", false);
 const PostsModel = require("./../models/posts");
-/* const { publicRoute } = require("../middlewares/publicRoute");
-const cookieParser = require("cookie-parser");
-router.use(cookieParser()); */
 
-/* router.get("/api/pages/:page", publicRoute, async (req, res) => { */
-router.get("/api/category/:category", async (req, res) => {
+router.get("/:category", async (req, res) => {
   try {
     const category = await PostsModel.find({
       //type: "post",
