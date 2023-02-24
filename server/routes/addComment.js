5484668258743d3a7aaeead9ca3bdfb616c799d4
Kevin Inzelrac <kevininzelrac@me.com>
2023-02-24 19:22:33 +0100

    Update addComment.js

--- server/routes/addComment.js
+++ server/routes/addComment.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
-mongoose.connect(process.env.HOST);
-mongoose.set("strictQuery", false);
 
 const CommentsModel = require("../models/comments");
 const { privateRoute } = require("../middlewares/privateRoute");
-/* const cookieParser = require("cookie-parser");
-router.use(cookieParser()); */
 
-router.post("/api/addComment", privateRoute, async (req, res) => {
+router.post("/", privateRoute, async (req, res) => {
   try {
     await new CommentsModel({
       author: await req.user._id,
