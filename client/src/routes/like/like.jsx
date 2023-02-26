5acd8fa4384d283b606ef1d728dffdde3419d173
Kevin Inzelrac <kevininzelrac@me.com>
2023-02-26 21:39:22 +0100

    Update like.jsx

--- client/src/routes/like/like.jsx
+++ client/src/routes/like/like.jsx
@@ -1,4 +1,25 @@
+import { io } from "socket.io-client";
+
 export async function likeAction({ request }) {
+  const formData = await request.formData();
+  const update = Object.fromEntries(formData);
+  const socket = io(location.origin);
+  socket.emit("like", update);
+  return null;
+}
+
+/* import { io } from "socket.io-client";
+export async function likeLoader() {
+  const socket = await io.connect(
+    new WebSocket(location.origin.replace(/^http/, "ws") + "/socket.io")
+  );
+  await socket.on("likes", async (data) => {
+    console.log(data);
+    return await data;
+  });
+} */
+
+/* export async function likeAction({ request }) {
   switch (request.method) {
     case "POST": {
       const formData = await request.formData();
@@ -25,4 +46,4 @@ export async function likeAction({ request }) {
       return data;
     }
   }
-}
+} */
