166c7c22af6c55da596e7eb4495012db654bc59e
Kevin Inzelrac <kevininzelrac@me.com>
2023-03-01 19:00:46 +0100

    Update save.jsx

--- client/src/components/slate/components/save.jsx
+++ client/src/components/slate/components/save.jsx
@@ -5,8 +5,6 @@ import Icon from "./icons";
 export default function Save({ label, about }) {
   const fetcher = useFetcher();
   const isIdle = fetcher.state === "idle";
-  const isSubmitting = fetcher.state === "submitting";
-  const isLoading = fetcher.state === "loading";
   return (
     <>
       <fetcher.Form method="post" action="Save">
@@ -16,7 +14,6 @@ export default function Save({ label, about }) {
           <Icon format="save" />
         </button>
       </fetcher.Form>
-      {fetcher.state}
       {fetcher && !isIdle && <p>sauvegarde en cours ...</p>}
     </>
   );
