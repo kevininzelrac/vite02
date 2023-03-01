93d4243d08957a05711b9619b9935bd5682d3429
Kevin Inzelrac <kevininzelrac@me.com>
2023-03-01 17:19:53 +0100

    Update parametres.jsx

--- client/src/routes/parametres/parametres.jsx
+++ client/src/routes/parametres/parametres.jsx
@@ -5,33 +5,32 @@ const Parametres = () => {
   const { user } = useRouteLoaderData("navbar");
   return (
     <div className="parametres">
-      <h2>Paramètres</h2>
-      <br />
       <form onSubmit={(e) => e.preventDefault()}>
-        <h3>{user.name}</h3>
-        <button>Modifier</button>
-        <br />
-        <br />
-        <span>{user.email}</span>
-        <br />
-        <button type="submit">Modifier</button>
-        <br />
-        <br />
+        <h2>Paramètres</h2>
+        <fieldset>
+          <label>{user?.name}</label>
+          <button>Modifier</button>
+        </fieldset>
 
-        <img width="80" src={user.avatar} />
-        <br />
-        <input type="file" />
-        <br />
-        <br />
-        <div>
-          <h4>Réinitialiser votre mot de passe</h4>
+        <fieldset>
+          <label>Email</label>
+          <span>{user?.email}</span>
+          <button type="submit">Modifier</button>
+        </fieldset>
+
+        <fieldset>
+          <label>Avatar</label>
+          <img width="80" src={user?.avatar} />
+          <input type="file" />
+        </fieldset>
+
+        <fieldset>
+          <label>Réinitialiser votre mot de passe</label>
           <input type="password" placeholder="New Password" />
-          <br />
           <input type="password" placeholder="Confirm New Password" />
-          <br />
-          <br />
-          <button type="submit">Enregistrer</button>
-        </div>
+        </fieldset>
+
+        <button type="submit">Enregistrer</button>
       </form>
     </div>
   );
