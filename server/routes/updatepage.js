bc33b79e09592fd7c76accb222e8a64ac12cd173
Kevin Inzelrac <kevininzelrac@me.com>
2023-03-01 18:02:23 +0100

    Update updatepage.js

--- server/routes/updatepage.js
+++ server/routes/updatepage.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
+
 const PostsModel = require("../models/posts");
 const { privateRoute } = require("../middlewares/privateRoute");
 
 router.post("/", privateRoute, async (req, res) => {
+  console.log(await req.body.about);
+  console.log(await req.body.label);
+  console.log(await req.user.name);
   try {
     const post = await PostsModel.findOne({
       author: await req.user.name,
