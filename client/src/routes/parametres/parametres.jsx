15dfe49b3e318940d9573175be7dc3e071221004
Kevin Inzelrac <kevininzelrac@me.com>
2023-03-02 02:38:43 +0100

    Update parametres.jsx

--- client/src/routes/parametres/parametres.jsx
+++ client/src/routes/parametres/parametres.jsx
@@ -3,33 +3,40 @@ import "./parametres.css";
 
 const Parametres = () => {
   const { user } = useRouteLoaderData("navbar");
+  const Todo = () => {
+    return <span style={{ color: "red" }}>Todo...</span>;
+  };
   return (
     <div className="parametres">
       <form onSubmit={(e) => e.preventDefault()}>
         <h2>Paramètres</h2>
         <fieldset>
-          <label>{user?.name}</label>
+          <label>
+            {user?.name} <Todo />
+          </label>
           <button>Modifier</button>
         </fieldset>
-
         <fieldset>
-          <label>Email</label>
+          <label>
+            Email <Todo />
+          </label>
           <span>{user?.email}</span>
           <button type="submit">Modifier</button>
         </fieldset>
-
         <fieldset>
-          <label>Avatar</label>
-          <img width="80" src={user?.avatar} />
+          <label>
+            Avatar <Todo />
+          </label>
+          <img width="100" src={user?.avatar} />
           <input type="file" />
         </fieldset>
-
         <fieldset>
-          <label>Réinitialiser votre mot de passe</label>
+          <label>
+            Réinitialiser votre mot de passe <Todo />
+          </label>
           <input type="password" placeholder="New Password" />
           <input type="password" placeholder="Confirm New Password" />
         </fieldset>
-
         <button type="submit">Enregistrer</button>
       </form>
     </div>
