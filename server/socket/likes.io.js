67736a1841b8e444ebdde1561fd4c04063a48827
Kevin Inzelrac <kevininzelrac@me.com>
2023-03-05 03:12:38 +0100

    add likes.io.js

---
+++ server/socket/likes.io.js
@@ -0,0 +1,24 @@
+const CommentsModel = require("../models/comments");
+
+module.exports = (io, socket) => {
+  socket.on("getLike", async () => {
+    const likes = await CommentsModel.find({}, { _id: 1, likes: 1 });
+    io.emit("likes", likes);
+  });
+
+  socket.on("like", async ({ comment_id, user_id }) => {
+    const comment = await CommentsModel.findOne({
+      _id: await comment_id,
+    });
+    if (comment.likes.includes(user_id)) {
+      const index = comment.likes.indexOf(await user_id);
+      comment.likes.splice(index, 1);
+      await comment.save();
+    } else {
+      comment.likes.push(await user_id);
+      await comment.save();
+    }
+    const likes = await CommentsModel.find({}, { _id: 1, likes: 1 });
+    io.emit("likes", likes);
+  });
+};
