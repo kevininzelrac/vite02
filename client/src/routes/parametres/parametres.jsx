36b1660a41d4cd0df8ccfc2cccf44cfc0ea7168a
Kevin Inzelrac <kevininzelrac@me.com>
2023-03-05 03:10:06 +0100

    update parametres.jsx

--- client/src/routes/parametres/parametres.jsx
+++ client/src/routes/parametres/parametres.jsx
@@ -1,44 +1,165 @@
-import { useRouteLoaderData } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useRouteLoaderData, useFetcher } from "react-router-dom";
 import "./parametres.css";
 
+export async function paramsAction({ request }) {
+  const formData = await request.formData();
+  const update = Object.fromEntries(formData);
+
+  const response = await fetch("/api/updateUser", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: "Bearer " + update.accessToken,
+    },
+    credentials: "include",
+    body: JSON.stringify(update),
+  });
+
+  const data = await response.json();
+  return await data;
+}
+
+const Input = ({ name, type, children }) => {
+  const [toggle, setToggle] = useState(true);
+  const [value, setValue] = useState(children);
+
+  return (
+    <fieldset>
+      <label
+        className={name}
+        style={{
+          animationName: toggle ? "jumpIn" : "jumpOut",
+        }}
+      >
+        {name}
+      </label>
+      <input
+        className={name}
+        style={{
+          animationName: toggle ? "jumpOut" : "jumpIn",
+        }}
+        type={type ?? "text"}
+        name={name}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          setToggle(!toggle);
+        }}
+      >
+        {toggle ? "Modifier" : "Aperçu"}
+      </button>
+    </fieldset>
+  );
+};
+
+const Password = () => {
+  const [password, setPassword] = useState("");
+  const [confirm, setConfirm] = useState("");
+  const [verified, setVerified] = useState("");
+  const [disabled, setDisable] = useState(true);
+  const fetcher = useFetcher();
+  useEffect(() => {
+    setPassword(""), setConfirm(""), setVerified(""), setDisable(true);
+  }, [fetcher.state.success]);
+
+  useEffect(() => {
+    password && password === confirm ? setDisable(false) : setDisable(true);
+  }, [password, confirm]);
+
+  return (
+    <fieldset className="password">
+      <label>
+        <small>
+          {verified
+            ? "Vos modifications sont pretes à être soumises"
+            : " Réinitialisez votre mot de passe"}
+        </small>
+      </label>
+      <input type="hidden" name="password" value={verified} />
+      <input
+        required
+        style={{
+          border: !password
+            ? ""
+            : password === confirm
+            ? "2px solid green"
+            : "2px solid red",
+        }}
+        type="password"
+        placeholder="New Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input
+        required
+        style={{
+          border: !confirm
+            ? ""
+            : password === confirm
+            ? "2px solid green"
+            : "2px solid red",
+        }}
+        type="password"
+        placeholder="Confirm New Password"
+        value={confirm}
+        onChange={(e) => setConfirm(e.target.value)}
+      />
+      <button
+        style={{
+          opacity: disabled ? "0.8" : "1",
+        }}
+        disabled={disabled}
+        onClick={(e) => {
+          e.preventDefault();
+          setVerified(password);
+        }}
+      >
+        Modifier
+      </button>
+    </fieldset>
+  );
+};
+
 const Parametres = () => {
-  const { user } = useRouteLoaderData("navbar");
-  const Todo = () => {
-    return <span style={{ color: "red" }}>Todo...</span>;
+  const { user, accessToken } = useRouteLoaderData("navbar");
+  const fetcher = useFetcher();
+  const idle = fetcher.state === "idle";
+  const error = fetcher.data?.error;
+  const success = fetcher.data?.success;
+
+  const handleSubmit = () => {
+    fetcher.submit({ accessToken: accessToken }, { method: "post" });
   };
+
   return (
     <div className="parametres">
-      <form onSubmit={(e) => e.preventDefault()}>
-        <h2>Paramètres</h2>
-        <fieldset>
-          <label>
-            {user?.name} <Todo />
-          </label>
-          <button>Modifier</button>
-        </fieldset>
-        <fieldset>
-          <label>
-            Email <Todo />
-          </label>
-          <span>{user?.email}</span>
-          <button type="submit">Modifier</button>
-        </fieldset>
-        <fieldset>
-          <label>
-            Avatar <Todo />
-          </label>
-          <img width="100" src={user?.avatar} />
-          <input type="file" />
-        </fieldset>
-        <fieldset>
-          <label>
-            Réinitialiser votre mot de passe <Todo />
-          </label>
-          <input type="password" placeholder="New Password" />
-          <input type="password" placeholder="Confirm New Password" />
-        </fieldset>
+      <h2>Paramètres</h2>
+      <header>
+        <img width="100" src={user?.avatar} />
+        <span>
+          <h3>{user.name}</h3>
+          <small> {user.email} </small>
+        </span>
+      </header>
+
+      <fetcher.Form method="post" onSubmit={handleSubmit}>
+        <Input name="avatar">{user.avatar}</Input>
+        <Input name="name">{user.name}</Input>
+        <Input name="email">{user.email}</Input>
+        <Password key={fetcher?.data?.success} />
+        {!idle ? (
+          fetcher.state
+        ) : error ? (
+          <label>{error}</label>
+        ) : (
+          <label>{success}</label>
+        )}
         <button type="submit">Enregistrer</button>
-      </form>
+      </fetcher.Form>
     </div>
   );
 };
