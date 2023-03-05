9984be35a0efd7e236aa6111bab2ec2f23d44a10
Kevin Inzelrac <kevininzelrac@me.com>
2023-03-05 03:03:31 +0100

    add footed.css

---
+++ client/src/components/footer/footer.jsx
@@ -0,0 +1,10 @@
+import "./footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <h4>Vite App 02</h4>
+      <small>Built by Kevin Inzelrac</small>
+    </footer>
+  );
+}
