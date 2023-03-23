b07f989bb1a0b2174b1928cccc1421d1ec396988
Kevin Inzelrac <kevininzelrac@me.com>
2023-03-23 20:51:16 +0100

    update lougout.js

--- server/routes/logout.js
+++ server/routes/logout.js
@@ -7,16 +7,21 @@ router.get("/", async (req, res) => {
   try {
     const refreshToken = await req.cookies["refreshToken"];
     const user = verify(refreshToken, process.env.REFRESH_TOKEN);
+
     await UsersModel.updateOne(
       { name: await user.name },
       { $set: { refreshToken: "" } }
     );
 
     res.cookie("refreshToken", "none", {
-      maxAge: new Date(Date.now() + 5 * 1000),
+      maxAge: new Date(Date.now() + 1 * 100),
       httpOnly: true,
     });
-    res.send({ message: "User logged out successfully" });
+    res.send({
+      /* user: "",
+      accessToken: "", */
+      message: "User logged out successfully",
+    });
   } catch (error) {
     res.send({ error: error });
   }
