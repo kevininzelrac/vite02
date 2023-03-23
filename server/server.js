c5fa4de1652ec21c8f5173b1391f3e5852b2b203
Kevin Inzelrac <kevininzelrac@me.com>
2023-03-23 22:41:10 +0100

    update server.js

--- server/server.js
+++ server/server.js
@@ -25,7 +25,8 @@ app.use(
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: process.env.ORIGIN,
+    //origin: process.env.ORIGIN,
+    origin: "*",
   },
 });
 
